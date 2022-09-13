fad29a716bdb1a6f1915a7fc033fd5b87445a8a9
Daniel Pereira <daniel.si@outlook.com.br>
2022-09-13 20:21:32 -0300

    Criando projeto Cloud parking

---
+++ src/main/java/com/dio/parking/CloundParkingApplication.java
@@ -0,0 +1,13 @@
+package com.dio.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloundParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloundParkingApplication.class, args);
+	}
+
+}
